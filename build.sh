5560d219b207fcb70967d84ef1713e44c7fac03d
Luis Rodriguez <luis@techreanimate.com>
2019-06-20 14:30:19 -0400

    Webhook Rebuild

--- build.sh
+++ build.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-# Delete folders to regenerate
-rm -rf ./docs/page/
-rm -rf ./docs/categories/
-rm -rf ./docs/post/
-rm -rf ./docs/samples/
-rm -rf ./docs/tags/
-
-# Build the project.
-hugo
-
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
+cd daemon/
+./daemon.run --daemon=false
---
+++ daemon/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+go build -o daemon.run daemon.go 
